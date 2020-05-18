574833da632b3c53f9d2b4d079cfa4720cf92444
İbrahim Karadağ <ibrahim.krdg98@hotmail.com>
2020-05-18 13:02:26 +0300

    Form Tasarım 0
    ->Renk seçildi
    ->Cansel butonu eklendi
    ->Ust panele tıklayınca sürükleme eklendi

---
+++ InstaBot/Formlar/AnaSayfa.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace InstaBot
+{
+    partial class AnaSayfa
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AnaSayfa));
+            this.pnlUst = new System.Windows.Forms.Panel();
+            this.pcCancel = new System.Windows.Forms.PictureBox();
+            this.pnlSol = new System.Windows.Forms.Panel();
+            this.pnlSag = new System.Windows.Forms.Panel();
+            this.pnlAyrac = new System.Windows.Forms.Panel();
+            this.pnlUst.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcCancel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlUst
+            // 
+            this.pnlUst.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(172)))), ((int)(((byte)(191)))), ((int)(((byte)(164)))));
+            this.pnlUst.Controls.Add(this.pcCancel);
+            this.pnlUst.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlUst.Location = new System.Drawing.Point(0, 0);
+            this.pnlUst.Name = "pnlUst";
+            this.pnlUst.Size = new System.Drawing.Size(862, 45);
+            this.pnlUst.TabIndex = 0;
+            this.pnlUst.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlUst_MouseDown);
+            this.pnlUst.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlUst_MouseMove);
+            this.pnlUst.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnlUst_MouseUp);
+            // 
+            // pcCancel
+            // 
+            this.pcCancel.Image = ((System.Drawing.Image)(resources.GetObject("pcCancel.Image")));
+            this.pcCancel.Location = new System.Drawing.Point(812, 5);
+            this.pcCancel.Name = "pcCancel";
+            this.pcCancel.Size = new System.Drawing.Size(38, 34);
+            this.pcCancel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcCancel.TabIndex = 0;
+            this.pcCancel.TabStop = false;
+            this.pcCancel.Click += new System.EventHandler(this.pcCancel_Click);
+            // 
+            // pnlSol
+            // 
+            this.pnlSol.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(172)))), ((int)(((byte)(191)))), ((int)(((byte)(164)))));
+            this.pnlSol.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlSol.Location = new System.Drawing.Point(0, 55);
+            this.pnlSol.Name = "pnlSol";
+            this.pnlSol.Size = new System.Drawing.Size(129, 475);
+            this.pnlSol.TabIndex = 3;
+            // 
+            // pnlSag
+            // 
+            this.pnlSag.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(232)))), ((int)(((byte)(206)))));
+            this.pnlSag.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlSag.Location = new System.Drawing.Point(129, 55);
+            this.pnlSag.Name = "pnlSag";
+            this.pnlSag.Size = new System.Drawing.Size(733, 475);
+            this.pnlSag.TabIndex = 2;
+            // 
+            // pnlAyrac
+            // 
+            this.pnlAyrac.BackColor = System.Drawing.Color.White;
+            this.pnlAyrac.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlAyrac.Location = new System.Drawing.Point(0, 45);
+            this.pnlAyrac.Name = "pnlAyrac";
+            this.pnlAyrac.Size = new System.Drawing.Size(862, 10);
+            this.pnlAyrac.TabIndex = 1;
+            // 
+            // AnaSayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 530);
+            this.Controls.Add(this.pnlSag);
+            this.Controls.Add(this.pnlSol);
+            this.Controls.Add(this.pnlAyrac);
+            this.Controls.Add(this.pnlUst);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AnaSayfa";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ana Sayfa";
+            this.pnlUst.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pcCancel)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlUst;
+        private System.Windows.Forms.Panel pnlSol;
+        private System.Windows.Forms.Panel pnlSag;
+        private System.Windows.Forms.Panel pnlAyrac;
+        private System.Windows.Forms.PictureBox pcCancel;
+    }
+}
+
---
+++ InstaBot/Formlar/AnaSayfa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InstaBot
+{
+    public partial class AnaSayfa : Form
+    {
+        public AnaSayfa()
+        {
+            InitializeComponent();
+        }
+
+        private void pcCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private bool mouseDown; //Formu sürüklemek için
+        private Point lastLocation; //Formu sürüklemek için
+        private void pnlUst_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true; //Formu sürüklemek için
+            lastLocation = e.Location; //Formu sürüklemek için
+        }
+
+        private void pnlUst_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown) //Formu sürüklemek için
+            {
+                this.Location = new Point((this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+                this.Update();
+            }
+        }
+
+        private void pnlUst_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false; //Formu sürüklemek için
+        }
+    }
+}
